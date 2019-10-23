f9f3103787282e5bae496512774499e80a069881
MAG <marcin.gala@ticos-systems.ch>
2019-10-23 14:19:16 +0200

    add database

---
+++ aveneo.API/Migrations/20191023111542_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aveneo.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BusinessEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    StreetNumber = table.Column<int>(nullable: false),
+                    PostCode = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    NIP = table.Column<string>(nullable: true),
+                    KRS = table.Column<string>(nullable: true),
+                    REGON = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BusinessEntities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BusinessEntities");
+        }
+    }
+}
