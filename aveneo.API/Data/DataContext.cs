f2d6eefd29180f7425665b4cd7778d491e7c2f05
MAG <marcin.gala@ticos-systems.ch>
2019-10-23 10:20:56 +0200

    second commit

---
+++ aveneo.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using aveneo.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aveneo.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BusinessEntity> BusinessEntities{ get; set; }
+    }
+}
---
+++ aveneo.API/Models/BusinessEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aveneo.API.Models
+{
+    public class BusinessEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string NIP { get; set; }
+        public string KRS { get; set; }
+        public string REGON { get; set; }
+    }
+}
