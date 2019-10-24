bd341ad7414a0db6d7ee912979f92b0d78fb43ad
ortofone24 <ortofone24@gmail.com>
2019-10-24 15:47:19 +0200

    add queary search

---
+++ aveneo.API/Controllers/AveneoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aveneo.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AveneoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "aveneo.API";
+        }
+    }
+}
---
+++ aveneo.API/Controllers/BusinessEntityController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aveneo.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aveneo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessEntityController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BusinessEntityController(DataContext context)
+        {
+            _context = context;
+        }
+
+       
+        [HttpGet("{searchQuery}")]
+        public async Task<IActionResult> Get(string searchQuery)
+        {
+            var result = await _context.BusinessEntities
+                                  .FirstOrDefaultAsync(x =>
+                                        x.KRS.Equals(searchQuery) ||
+                                        x.NIP.Equals(searchQuery) ||
+                                        x.REGON.Equals(searchQuery));
+
+            return Ok(result);
+        }
+    }
+}
